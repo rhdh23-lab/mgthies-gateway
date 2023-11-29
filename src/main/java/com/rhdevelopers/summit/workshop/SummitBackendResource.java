ad012d603fd4f2a38b7c9378e0e10251a8dc1813
null <null>
2023-11-29 10:19:43 +0000

    code a new feature through removing comments :-)

--- src/main/java/com/rhdevelopers/summit/workshop/SummitBackendResource.java
+++ src/main/java/com/rhdevelopers/summit/workshop/SummitBackendResource.java
@@ -70,7 +70,6 @@ public class SummitBackendResource {
 
     //Uncomment the Java method below to add an API endpoint to add your own data points.
     
-    /*
     @POST
     @Path("poi/add/point")
     public Response loadCustomPoi(PoiRecord record) {
@@ -79,8 +78,7 @@ public class SummitBackendResource {
             UriBuilder.fromPath("fake/poi/find/"+(SUMMIT_POI_RECORDS.size()-1)).build()
         ).entity("inserted custom data point").build();
     }
-    */
-
+ 
     //How to use it:
     //The coordinates number array in the JSON snippet is specified [lat,lng] -> so latitude first, then longitude.
     //1. You can retrieve the GPS coordinates e.g. for your home town or some other city from here https://www.latlong.net/
